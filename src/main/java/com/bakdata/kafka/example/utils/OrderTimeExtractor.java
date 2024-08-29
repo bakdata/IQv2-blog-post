0f4815b19a3db2a76ec216f319248b7c19d2c435
Ramin Gharib <ramin.gharib@bakdata.com>
2024-08-29 10:19:03 +0200

    clean up

--- src/main/java/com/bakdata/kafka/example/utils/OrderTimeExtractor.java
+++ src/main/java/com/bakdata/kafka/example/utils/OrderTimeExtractor.java
@@ -1,8 +1,6 @@
 package com.bakdata.kafka.example.utils;
 
 import com.bakdata.kafka.example.model.Order;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.streams.processor.TimestampExtractor;
 
@@ -10,16 +8,10 @@ import java.util.Optional;
 
 public class OrderTimeExtractor implements TimestampExtractor {
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
-
     @Override
     public long extract(final ConsumerRecord<Object, Object> record, final long partitionTime) {
-        try {
-            final Order order = MAPPER.readValue((String) record.value(), Order.class);
-            return Optional.of(order.timestamp())
-                    .orElse(partitionTime);
-        } catch (final JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        final Order order = Utils.readToObject((String) record.value(), Order.class);
+        return Optional.of(order.timestamp())
+                .orElse(partitionTime);
     }
 }
